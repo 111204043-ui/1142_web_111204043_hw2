8c3eab5dd6563577d2152cf0f3df4f0e988c810d
王浩川 <111204043@g.nccu.edu.tw>
2026-05-26 23:51:59 +0800

    feat: building

---
+++ component/ActionButton.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Link from "next/link";
+
+export default function ActionButton() {
+
+  return (
+    <>
+        <Link className="text-white bg-black px-3 py-2" href="/">button</Link>
+    </>
+  );
+}
